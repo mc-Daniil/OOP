b1f6a19496566e152e0de8b4daf0f786e79ca534
rudakov.da <rudakov.da@dozen.mephi.ru>
2024-09-05 14:50:11 +0300

    Main, getChoice and empty functions

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,29 +1,82 @@
 #include <iostream>
+#include <limits>
+
 using std::cin;
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::string;
-#define PROMPT "Выберите функцию: 1, 2, 3, 4"
+using std::runtime_error;
+#define PROMPT "Choose function: 1, 2, 3, 4:"
 
-int getChoice(){
-    int choice;
+
+auto checkInp(int &choice) -> bool{
+    if (choice < 0 || choice > 4) {
+        return false;
+    }
+    return true;
+}
+
+
+auto getChoice(int &choice) -> bool{
     cout << PROMPT << endl;
     cin >> choice;
-    return choice;
-}
-
-
-auto join(string absolute, string relative) -> int{
-    return 0;
-}
-
-
-auto main() -> int{
-    int choice(getChoice());
-    while (choice) {
-        cout << choice << endl;
-        choice = getChoice();
+    if (cin.good() && checkInp(choice)) {
+        return true;
     }
+    if (cin.eof()) {
+        throw runtime_error("Got EOF");
+    }
+    if (cin.bad()) {
+        throw runtime_error("Got bad error");
+    }
+    if (cin.fail() || !checkInp(choice)) {
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cerr << "Wrong input. Try again" << endl;
+    }
+    return false;
+}
 
-    return 0;
+
+auto join() -> bool{
+    cout << 1 << endl;
+    return true;
+}
+
+
+auto absolute() -> bool{
+    cout << 2 << endl;
+    return true;
+}
+
+
+auto relative() -> bool{
+    cout << 3 << endl;
+    return true;
+}
+
+
+auto relativize() -> bool{
+    cout << 4 << endl;
+    return true;
+}
+
+
+auto finish() -> bool{
+    cout << 0 << endl;
+    return true;
+}
+
+
+auto main() -> int {
+    bool (*funcs[5])() = {finish, join, absolute, relative, relativize};
+    try {
+        int choice;
+        while (!getChoice(choice));
+        funcs[choice]();
+    }
+    catch (const std::exception &e) {
+        cerr << "Error:" << e.what() << endl;
+    }
 }
