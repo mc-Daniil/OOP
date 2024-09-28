5cc0b4b08be7751a56988adb4a455fe43d5f91a4
rudakov.da <rudakov.da@dozen.mephi.ru>
2024-09-28 22:12:09 +0300

    Tests for overloads

--- 1/tests.cpp
+++ 1/tests.cpp
@@ -23,6 +23,8 @@ TEST_CASE("checkChoice") {
 }
 
 TEST_CASE("checkPath") {
+    REQUIRE(checkPath(std::string("valid\\path")) == true);
+    REQUIRE(checkPath(std::string("another\\valid\\path")) == true);
     REQUIRE(checkPath("valid\\path") == true);
     REQUIRE(checkPath("another\\valid\\path") == true);
 
@@ -33,15 +35,15 @@ TEST_CASE("checkPath") {
 }
 
 TEST_CASE("join function") {
-    REQUIRE(join("D:\\code\\folder", "file.txt") == "D:\\code\\folder\\file.txt");
-    REQUIRE(join("D:\\code\\folder\\", "file.txt") == "D:\\code\\folder\\file.txt");
+    // Проверка с std::string
+    REQUIRE(join(std::string("D:\\code\\folder"), std::string("file.txt")) == "D:\\code\\folder\\file.txt");
+    REQUIRE(join(std::string("D:\\code\\folder\\"), std::string("file.txt")) == "D:\\code\\folder\\file.txt");
 
-    REQUIRE_THROWS_AS(join("D:\\code\\folder", ""), std::invalid_argument);
-    REQUIRE_THROWS_AS(join("D:\\code\\folder", "\\file.txt"), std::invalid_argument);
-    REQUIRE_THROWS_AS(join("D:\\code\\folder", nullptr), std::invalid_argument);
-}
+    REQUIRE_THROWS_AS(join(std::string("D:\\code\\folder"), std::string("")), std::invalid_argument);
+    REQUIRE_THROWS_AS(join(std::string("D:\\code\\folder"), std::string("\\file.txt")), std::invalid_argument);
+    REQUIRE_THROWS_AS(join(std::string("D:\\code\\folder"), ""), std::invalid_argument);
 
-TEST_CASE("join function with char*") {
+    // Проверка с const char*
     REQUIRE(join("D:\\code\\folder", "file.txt") == "D:\\code\\folder\\file.txt");
     REQUIRE(join("D:\\code\\folder\\", "file.txt") == "D:\\code\\folder\\file.txt");
 
@@ -53,50 +55,39 @@ TEST_CASE("join function with char*") {
 TEST_CASE("absolute function") {
     auto current_path = std::filesystem::current_path().string();
 
+    // Проверка с std::string
+    REQUIRE(absolute(std::string("file.txt")).starts_with(current_path + "\\"));
+    REQUIRE(absolute(std::string("subfolder\\file.txt")).starts_with(current_path + "\\subfolder"));
+    REQUIRE(absolute(std::string("C:\\file.txt")) == "C:\\file.txt");
+
+    // Проверка с const char*
     REQUIRE(absolute("file.txt").starts_with(current_path + "\\"));
     REQUIRE(absolute("subfolder\\file.txt").starts_with(current_path + "\\subfolder"));
-
-    REQUIRE(absolute("C:\\file.txt") == "C:\\file.txt");
-}
-
-TEST_CASE("absolute function with char*") {
-    auto current_path = std::filesystem::current_path().string();
-
-    REQUIRE(absolute("file.txt").starts_with(current_path + "\\"));
-    REQUIRE(absolute("subfolder\\file.txt").starts_with(current_path + "\\subfolder"));
-
     REQUIRE(absolute("C:\\file.txt") == "C:\\file.txt");
 }
 
 TEST_CASE("relative function") {
     auto current_path = std::filesystem::current_path().string();
 
-    REQUIRE(relative(current_path + "\\file.txt") == "file.txt");
+    // Проверка с std::string
+    REQUIRE(relative(std::string(current_path + "\\file.txt")) == "file.txt");
+    REQUIRE(relative(std::string(current_path + "\\subfolder\\file.txt")).starts_with("subfolder"));
+    REQUIRE(relative(std::string("C:\\folder\\file.txt")).find("..") != std::string::npos);
 
-    REQUIRE(relative(current_path + "\\subfolder\\file.txt").starts_with("subfolder"));
-
-    REQUIRE(relative("C:\\folder\\file.txt").find("..") != std::string::npos);
-}
-
-TEST_CASE("relative function with char*") {
-    auto current_path = std::filesystem::current_path().string();
-
+    // Проверка с const char*
     REQUIRE(relative(current_path + "\\file.txt") == "file.txt");
     REQUIRE(relative(current_path + "\\subfolder\\file.txt").starts_with("subfolder"));
-
     REQUIRE(relative("C:\\folder\\file.txt").find("..") != std::string::npos);
 }
 
 TEST_CASE("relativize function") {
+    // Проверка с std::string
+    REQUIRE(relativize(std::string("D:\\code\\folder"), std::string("D:\\code\\folder\\file.txt")) == "file.txt");
+    REQUIRE_THROWS_AS(relativize(std::string("D:\\code\\folder"), std::string("E:\\folder\\file.txt")), std::invalid_argument);
+    REQUIRE_THROWS_AS(relativize(std::string("D:\\code\\folder"), std::string("D:\\code\\other\\file.txt")), std::invalid_argument);
+
+    // Проверка с const char*
     REQUIRE(relativize("D:\\code\\folder", "D:\\code\\folder\\file.txt") == "file.txt");
-
-    REQUIRE_THROWS_AS(relativize("D:\\code\\folder", "E:\\folder\\file.txt"), std::invalid_argument);
-    REQUIRE_THROWS_AS(relativize("D:\\code\\folder", "D:\\code\\other\\file.txt"), std::invalid_argument);
-}
-
-TEST_CASE("relativize function with char*") {
-    REQUIRE(relativize("D:\\code\\folder", "D:\\code\\folder\\file.txt") == "file.txt");
-
     REQUIRE_THROWS_AS(relativize("D:\\code\\folder", "E:\\folder\\file.txt"), std::invalid_argument);
     REQUIRE_THROWS_AS(relativize("D:\\code\\folder", "D:\\code\\other\\file.txt"), std::invalid_argument);
 }
@@ -108,6 +99,6 @@ TEST_CASE("absolute with root directory") {
 
 TEST_CASE("relative paths at different levels") {
     auto current_path = std::filesystem::current_path().string();
-    REQUIRE(relative(current_path + "\\folder1\\folder2\\file.txt").find("..") == std::string::npos);
+    REQUIRE(relative(std::string(current_path + "\\folder1\\folder2\\file.txt")).find("..") == std::string::npos);
     REQUIRE(relative("C:\\folder1\\folder2\\file.txt").find("..") != std::string::npos);
 }
